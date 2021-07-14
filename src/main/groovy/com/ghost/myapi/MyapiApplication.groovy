0d1294d2759e8fb4b3093472f1cda37965b13449
Widiyantoko <widiytk@gmail.com>
2021-07-14 17:30:38 +0800

    initial commit

---
+++ src/main/groovy/com/ghost/myapi/MyapiApplication.groovy
@@ -0,0 +1,13 @@
+package com.ghost.myapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MyapiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MyapiApplication, args)
+	}
+
+}
